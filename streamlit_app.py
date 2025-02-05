f73a30b40fb683f712045b0f4e8c18da8272c343
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2025-02-05 17:13:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 from together import Together
 
+client = Together(api_key="22c7d3723a5c143b4f1ac02fd15b5d0d5034629c91abe3281d2f1e029c2aa371")
+
+response = client.chat.completions.create(
+    model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
+    messages=[{"role": "user", "content": "What are some fun things to do in New York?"}],
+)
+
 st.title("Asti")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    f"Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
