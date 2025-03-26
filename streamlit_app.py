ecdad03ced327ef72491ac17e6eea044dbd4a31a
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2025-03-26 19:46:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,11 @@ st.set_page_config(
 
 # Sidebar navigation
 st.sidebar.page_link("streamlit_app.py", label="Chat", icon="💬")
-with st.sidebar.expander("Legal and Support"):
-    st.page_link("pages/Terms_&_Conditions.py", label="Terms & Conditions", icon="📜")
-    st.page_link("pages/Privacy_Policy.py", label="Privacy policy", icon="🛡️")
-    st.page_link("pages/About_Us.py", label="About Us", icon="ℹ️")
-    st.page_link("pages/Refund_policy.py", label="Refund policy", icon="🔄")
-    st.page_link("pages/Contact_Us.py", label="Contact Us", icon="📞")
+if st.sidebar.button("🗑️ Clear Chat"):
+    st.session_state.messages = []
+    st.session_state.document_content = None
+    st.rerun()
+
 
 # Initialize Together client
 api_key = st.secrets["API_KEY"]
